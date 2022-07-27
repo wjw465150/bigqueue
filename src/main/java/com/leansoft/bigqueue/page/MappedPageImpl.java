bbd528a37c7835b8da7322f2c2b80d8b04162ccb
wjw465150 <wjw465150@gmail.com>
2022-07-27 22:12:06 +0800

    MappedByteBuffer not release due to JDK11 does not support `sun.misc.Cleaner`

--- src/main/java/com/leansoft/bigqueue/page/MappedPageImpl.java
+++ src/main/java/com/leansoft/bigqueue/page/MappedPageImpl.java
@@ -2,11 +2,13 @@ package com.leansoft.bigqueue.page;
 
 import java.io.Closeable;
 import java.io.IOException;
-import java.lang.reflect.Method;
+import java.lang.reflect.Field;
 import java.nio.ByteBuffer;
 import java.nio.MappedByteBuffer;
 
 import org.apache.log4j.Logger;
+
+import sun.misc.Unsafe;
 
 public class MappedPageImpl implements IMappedPage, Closeable {
 	
@@ -85,40 +87,29 @@ public class MappedPageImpl implements IMappedPage, Closeable {
      * Helper class allowing to clean direct buffers.
      */
     private static class Cleaner {
-        public static final boolean CLEAN_SUPPORTED;
-        private static final Method directBufferCleaner;
-        private static final Method directBufferCleanerClean;
+		private static Unsafe unsafe;
 
-        static {
-            Method directBufferCleanerX = null;
-            Method directBufferCleanerCleanX = null;
-            boolean v;
-            try {
-                directBufferCleanerX = Class.forName("java.nio.DirectByteBuffer").getMethod("cleaner");
-                directBufferCleanerX.setAccessible(true);
-                directBufferCleanerCleanX = Class.forName("sun.misc.Cleaner").getMethod("clean");
-                directBufferCleanerCleanX.setAccessible(true);
-                v = true;
-            } catch (Exception e) {
-                v = false;
-            }
-            CLEAN_SUPPORTED = v;
-            directBufferCleaner = directBufferCleanerX;
-            directBufferCleanerClean = directBufferCleanerCleanX;
-        }
+		static {
+			try {
+				Field f = Unsafe.class.getDeclaredField("theUnsafe");
+				f.setAccessible(true);
+				unsafe = (Unsafe) f.get(null);
+			} catch (Exception e) {
+				logger.warn("Unsafe Not support "+ e.getMessage(), e);
+			}
+		}
 
-        public static void clean(ByteBuffer buffer) {
-    		if (buffer == null) return;
-            if (CLEAN_SUPPORTED && buffer.isDirect()) {
-                try {
-                    Object cleaner = directBufferCleaner.invoke(buffer);
-                    directBufferCleanerClean.invoke(cleaner);
-                } catch (Exception e) {
-                    // silently ignore exception
-                }
-            }
-        }
-    }
+		public static void clean(ByteBuffer buffer) {
+			if (buffer == null) return;
+			if (buffer.isDirect()) {
+				if (unsafe != null) {
+					unsafe.invokeCleaner(buffer);
+				} else {
+					logger.warn("Unable to clean bytebuffer");
+				}
+			}
+		}
+	}
     
     private static class ThreadLocalByteBuffer extends ThreadLocal<ByteBuffer> {
     	private ByteBuffer _src;
